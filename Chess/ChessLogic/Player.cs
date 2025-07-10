eace2b176b99017b731dbd8ba9d8c6192ac2e3cb
Doan Hoang Anh <98464268+hganhdoan0804@users.noreply.github.com>
2025-07-10 08:30:56 +0700

    Set up

---
+++ Chess/ChessLogic/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public enum Player
+    {
+        None,
+        White,
+        Black
+    }
+
+    public static class PlayerExtensions
+    {
+        public static Player Opposite(this Player player)
+        {
+            return player switch
+            {
+                Player.White => Player.Black,
+                Player.Black => Player.White,
+                _ => Player.None
+            };
+        }
+    }
+}
